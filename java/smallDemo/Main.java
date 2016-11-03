82f375b7d9cc582b77650fbbd30f298fef17f8b8
lsj <lsj9383@163.com>
2016-11-03 23:41:22 +0800

    update

---
+++ java/smallDemo/Main.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		List<String> list = new ArrayList<String>();
+		list.add("hello");
+		System.out.println(list.get(0));
+	}
+}
