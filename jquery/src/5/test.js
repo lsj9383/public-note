f32ede08512844e724936ad6ef192e972ef2dd23
lsj <lsj9383@163.com>
2016-12-21 18:21:02 +0800

    update mybatis

--- jquery/src/5/test.js
+++ jquery/src/5/test.js
@@ -46,3 +46,14 @@ $(document).ready(function(){
 		.wrap('<li id="footnote-'+(index+1)+'"></li>');
 	});
 });
+
+$(document).ready(function(){
+	$('span.pull-quote').each(function(index){
+		var $parentParagraph = $(this).parent('p');
+		$parentParagraph.css('position', 'relative');
+		var $clonedCopy = $(this).clone();
+		$clonedCopy
+			.addClass('pulled')
+			.prependTo($parentParagraph);
+	});
+});
