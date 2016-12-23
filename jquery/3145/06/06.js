d806b24432e32a796a391c2c1a24f869de20d76e
lsj <lsj9383@163.com>
2016-12-23 14:41:20 +0800

    update

--- jquery/3145/06/06.js
+++ jquery/3145/06/06.js
@@ -4,3 +4,9 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(document).ready(function() {
+$('#letter-a a').click(function(event) {
+event.preventDefault();
+$('#dictionary').load('a.html');
+});
+});
