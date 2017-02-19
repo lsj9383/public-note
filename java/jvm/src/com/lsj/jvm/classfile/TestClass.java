4951b9ef33ff6220990c359421685acad02097df
lsj <lsj9383@163.com>
2017-02-19 21:56:25 +0800

    update collection

--- java/jvm/src/com/lsj/jvm/classfile/TestClass.java
+++ java/jvm/src/com/lsj/jvm/classfile/TestClass.java
@@ -3,8 +3,22 @@ package com.lsj.jvm.classfile;
 public class TestClass{
 	
 	private int m;
-	public int inc(){
+	public int fun(){
+		privMethod();
+		publicMethod();
+		staticPublicMethod();
 		return m+1;
 	}
 	
+	private void privMethod(){
+		System.out.println("private");
+	}
+	
+	public void publicMethod(){
+		System.out.println("public");
+	}
+	
+	static public void staticPublicMethod(){
+		System.out.println("static public");
+	}
 }
