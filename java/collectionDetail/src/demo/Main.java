1f2c1d856013481a08429920b68f76152f227247
lsj <lsj9383@163.com>
2017-02-21 23:22:19 +0800

    update

--- java/collectionDetail/src/demo/Main.java
+++ java/collectionDetail/src/demo/Main.java
@@ -1,26 +1,18 @@
 package demo;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<Integer> list = new LinkedList<>();
-		Set<Integer> sets = new HashSet<Integer>();
+		List<? extends Fruit> flist = new ArrayList<Apple>();
 		
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		list.add(5);
-	
-		((LinkedList<Integer>)list).clone();
 	}
 
 }
+
+
+class Fruit {}
+class Apple extends Fruit {}
+class Orange extends Fruit{}
