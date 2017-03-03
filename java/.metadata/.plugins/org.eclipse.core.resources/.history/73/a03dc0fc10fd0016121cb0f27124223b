3414566ffb680a8f03b7f8bfd03b53f81fc0a71b
lsj <lsj9383@163.com>
2017-03-03 20:50:24 +0800

    update note

---
+++ java/.metadata/.plugins/org.eclipse.core.resources/.history/73/a03dc0fc10fd0016121cb0f27124223b
@@ -0,0 +1,5 @@
+package com.lsj.util;
+
+public interface LIterable<T> {
+	LIterator<T> iterator();
+}
---
+++ java/collectionDetail/src/com/lsj/util/LEntry.java
@@ -0,0 +1,6 @@
+package com.lsj.util;
+
+public interface LEntry<K, V> {
+	K getKey();
+	V getValue();
+}
