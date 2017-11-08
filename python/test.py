72719fef44aa68a704bd4235126d72a41050f921
lsj <lsj9383@163.com>
2017-11-08 16:02:03 +0800

    update

--- python/test.py
+++ python/test.py
@@ -1,2 +1,20 @@
-li = [item for item in range(10) if item%2==0];
-print(li)
+class Person():
+	count = 0;
+	def __init__(self, name):
+		self._name = name;
+		self.__age = 18;
+		self.count+=1;
+		Person.count+=1;
+		
+	def get_name(self):
+		print("read");
+		return self._name;
+		
+	def set_name(self, name):
+		print("write");
+		self._name = name;
+		
+me = Person("lsj");
+he = Person("hjs");
+print(me.count);
+print(he.count);
